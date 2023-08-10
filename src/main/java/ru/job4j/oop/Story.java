3cb0e66343a00dbc18824d30cb44f04fc320f97c
Игорь <hardplayer455@gmail.com>
2023-08-10 19:34:07 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/oop/Story.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+public class Story {
+    private static void ballsStory() {
+        Ball ball = new Ball();
+        Wolf wolf = new Wolf();
+        Hare hare = new Hare();
+        Fox fox = new Fox();
+        wolf.tryEat(ball);
+        hare.tryEat(ball);
+        fox.tryEat(ball);
+    }
+
+    public static void main(String[] args) {
+        Pioneer petya = new Pioneer();
+        Girl girl = new Girl();
+        Wolf wolf = new Wolf();
+        girl.help(petya);
+        wolf.eat(girl);
+        petya.kill(wolf);
+        ballsStory();
+    }
+}
