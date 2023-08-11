153b116269c742dc7808a9a7fa32e280138dd99e
Игорь <hardplayer455@gmail.com>
2023-08-11 13:27:34 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/oop/Ball.java
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -2,6 +2,7 @@ package ru.job4j.oop;
 
 public class Ball {
     public void tryRun(boolean condition) {
+
         if (condition) {
             System.out.println("Колобок сбежал!");
         } else {
