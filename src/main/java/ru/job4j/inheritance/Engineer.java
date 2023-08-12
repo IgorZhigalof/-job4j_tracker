44aa8b2335e84b2efba51bd33f16cb159e8921c1
Игорь <hardplayer455@gmail.com>
2023-08-12 19:12:35 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/inheritance/Engineer.java
+++ src/main/java/ru/job4j/inheritance/Engineer.java
@@ -3,8 +3,8 @@ package ru.job4j.inheritance;
 public class Engineer extends Profession {
     private int experience;
 
-    public Engineer(int experience, boolean degree) {
-        super(degree);
+    public Engineer(int experience, boolean degre) {
+        super(degre);
         this.experience = experience;
     }
 }
