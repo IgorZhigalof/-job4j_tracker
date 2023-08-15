10c2f4177fc97e0b8df765ad42ea04253d2537c5
Игорь <hardplayer455@gmail.com>
2023-08-15 16:34:37 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -0,0 +1,17 @@
+package ru.job4j.io;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String text = input.nextLine();
+        int answer = new Random().nextInt(3);
+        switch (answer) {
+            case 0 -> System.out.println("Да");
+            case 1 -> System.out.println("Нет");
+            default -> System.out.println("Может быть");
+        }
+    }
+}
