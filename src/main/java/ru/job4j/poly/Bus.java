42220ca13ba792092a6cc39f5395ae9e088451dc
Игорь <hardplayer455@gmail.com>
2023-08-16 21:10:06 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,6 +1,6 @@
 package ru.job4j.poly;
 
-public class Bus implements Transport {
+public class Bus implements ru.job4j.poly.Transport {
     @Override
     public void move() {
         System.out.println("Moving forward");
