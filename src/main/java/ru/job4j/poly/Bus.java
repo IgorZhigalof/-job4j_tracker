41bb26eabcaf89c43ef35c42d47c18a74320d540
Игорь <hardplayer455@gmail.com>
2023-08-16 19:58:28 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void move() {
+        System.out.println("Moving forward");
+    }
+
+    @Override
+    public void passengers(int amount) {
+        System.out.println("There's " + amount + " passengers");
+    }
+
+    @Override
+    public double refuel(double quantity) {
+        System.out.println("Refueled " + quantity + "liters of fuel");
+        return quantity * 1000;
+    }
+}
