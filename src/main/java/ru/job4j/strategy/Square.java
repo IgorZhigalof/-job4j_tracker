714926a76fcb385833d2ff76a9c55834ead95ffb
Игорь <hardplayer455@gmail.com>
2023-08-17 20:17:35 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -4,7 +4,7 @@ public class Square implements Shape {
     @Override
     public String draw() {
         String ln = System.lineSeparator();
-        return    "_____________" + ln
+        return    "_____________ " + ln
                 + "|           |" + ln
                 + "|           |" + ln
                 + "|           |" + ln
