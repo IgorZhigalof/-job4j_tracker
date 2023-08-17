174028c7d80b1b77dc80187563bd235e8786ec0d
Игорь <hardplayer455@gmail.com>
2023-08-17 14:56:56 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -53,7 +53,7 @@ public class StartUI {
         System.out.println("=== Delete item ===");
         int id = input.askInt("Enter id: ");
         if (tracker.delete(id)) {
-            System.out.println("Заявка удалена успешно.");
+            System.out.println("Заявка удалена успешно. ");
         } else {
             System.out.println("Ошибка удаления заявки.");
         }
