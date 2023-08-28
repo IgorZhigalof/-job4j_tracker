c9fc2ff06858aa96cf890e0ff607fceb7cb208f4
Игорь <hardplayer455@gmail.com>
2023-08-28 13:57:53 +0300

    0. Map, HashMap [#212652 #439879] Исправления №1

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("igor.zhigalof@gmail.com", "Zhigalov Igor");
+        hashMap.put("random.mail@gmail.com", "any info");
+        hashMap.put("anotherrandommail@gmail.com", "any info");
         hashMap.put("igor.zhigalof@gmail.com", "Zhigalov Igor Olegovych");
         for (String key : hashMap.keySet()) {
             String value = hashMap.get(key);
