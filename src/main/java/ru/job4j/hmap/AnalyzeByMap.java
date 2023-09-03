ea6530e095efa9147763a5da91a9c0bfd4a4131b
Игорь <hardplayer455@gmail.com>
2023-09-03 16:59:58 +0300

    4. Аттестация [#504878 #441300] Перезалив коммита

--- src/main/java/ru/job4j/hmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hmap/AnalyzeByMap.java
@@ -52,7 +52,7 @@ public class AnalyzeByMap {
         HashMap<String, Integer> totalScore = new LinkedHashMap<>();
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
-                totalScore.put(subject.name(), totalScore.getOrDefault(subject.name(), 0) + subject.score());
+                totalScore.put(subject.name(), totalScore.getOrDefault(subject.name(), -1) + subject.score());
             }
         }
         return totalScore;
