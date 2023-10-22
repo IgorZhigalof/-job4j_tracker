d3ea804182a53a75e4230c631fb2b9ee12c3365e
Игорь <hardplayer455@gmail.com>
2023-10-22 13:08:34 +0300

    2. Local-Variable Type Inference [#70626 #454425] Перезалив

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.function.Predicate;
 
 public class PhoneDictionary {
+
     private ArrayList<Person> persons = new ArrayList<>();
 
     public void add(Person person) {
